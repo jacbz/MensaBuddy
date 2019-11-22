27fa1f1d41178a4e07f91fadf45b08c729d37bf1
Jacob Zhang <jacob-z@live.de>
2019-11-23 00:51:56 +0100

    Add basic front page layout

--- app/src/main/java/de/jzjh/mensabuddy/ui/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package de.jzjh.mensabuddy.ui.login
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult (
-     val success:LoggedInUserView? = null,
-     val error:Int? = null
-)
